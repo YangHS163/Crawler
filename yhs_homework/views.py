c3c8ffd7c7a69e87764d8c0682dc6e7fa4bcce4c
YangHS163 <409550059@qq.com>
2019-03-20 23:23:22 +0800

    Add files via upload

--- yhs_homework/views.py
+++ yhs_homework/views.py
@@ -21,17 +21,17 @@ class BeautifulPicture():
         #self.folder_path = 'D:\CNN_NEWs'
 
     def get_pic(self):
-        print('开始网页get请求')
+        print('Starting')
 
         driver = webdriver.PhantomJS()
-        print('ok1')
+        #print('ok1')
         df = pd.DataFrame(columns=('index', 'href', 'heading', 'time'))
         mytime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         df.loc[1, 'time'] = mytime
         driver.get(self.web_url)
-        print('ok2')
+        #print('ok2')
         self.scroll_down(driver=driver, times=1)  #执行网页下拉到底部操作
-        print('开始获取所有h3标签')
+        print('gathering all h3 Tag ')
         page_one = BeautifulSoup(driver.page_source, 'html.parser').find_all('h3', class_='cnn-search__result-headline')  #获取网页中的搜索结果的所有h3标签
 
 
@@ -43,11 +43,6 @@ class BeautifulPicture():
            df.loc[index, 'index'] = index
            df.loc[index, 'href'] = item1['href'][2:]
            df.loc[index, 'heading'] = item1.contents[0]
-           #print(item1['href'])
-           #print(item1.contents)
-
-        #driver.switch_to_frame(1)
-        #print(driver.find_element_by_class_name('icon icon--arrow-right'))
         driver.find_element_by_class_name('pagination-arrow-right').click()
         self.scroll_down(driver=driver, times=1)
         page_two = BeautifulSoup(driver.page_source, 'html.parser').find_all('h3', class_='cnn-search__result-headline')
@@ -57,8 +52,6 @@ class BeautifulPicture():
            df.loc[index, 'index'] = index
            df.loc[index, 'href'] = item2['href'][2:]
            df.loc[index, 'heading'] = item2.contents[0]
-           # print(item2['href'])
-           # print(item2.contents)
 
         driver.find_element_by_class_name('pagination-arrow-right').click()
         self.scroll_down(driver=driver, times=1)
@@ -69,44 +62,12 @@ class BeautifulPicture():
             df.loc[index, 'index'] = index
             df.loc[index, 'href'] = item3['href'][2:]
             df.loc[index, 'heading'] = item3.contents[0]
-            # print(item3['href'])
-            # print(item3.contents)
 
-        #print('开始创建文件夹')
-        #is_new_folder = self.mkdir(self.folder_path)
-        #print('开始切换文件夹')
-        #os.chdir(self.folder_path)   #切换路径至上面创建的文件夹
-
-        print("提取的新闻数量是：", len(page_one)+len(page_two)+len(page_three))   #Check Total Number
+        print("The number of Extracted News", len(page_one)+len(page_two)+len(page_three))   #Check Total Number
         #file_names = self.get_files(self.folder_path)  #获取文件家中的所有文件名，类型是list
         mytime = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
         df.to_excel('cnn_search.xls')
 
-
-    def save_img(self, url, file_name): ##保存图片
-        #print('开始请求图片地址，过程会有点长...')
-        img = self.request(url)
-        print('开始保存图片')
-        f = open(file_name, 'ab')
-        f.write(img.content)
-        print(file_name,'图片保存成功！')
-        f.close()
-
-    def request(self, url):  #返回网页的response
-        r = requests.get(url)  # 像目标url地址发送get请求，返回一个response对象。有没有headers参数都可以。
-        return r
-
-    def mkdir(self, path):  ##这个函数创建文件夹
-        path = path.strip()
-        isExists = os.path.exists(path)
-        if not isExists:
-            print('创建名字叫做', path, '的文件夹')
-            os.makedirs(path)
-            print('创建成功！')
-            return True
-        else:
-            print(path, '文件夹已经存在了，不再创建')
-            return False
 
     def scroll_down(self, driver, times):
         print('ok3')
